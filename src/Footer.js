968aa005e2cecd86e137bc1ce3e71a7be20e3caf
nitin-codr <nitinporwal99@gmail.com>
2020-06-12 09:10:44 +0530

    add styling in footer page with classes and remove inline styling

--- src/Footer.js
+++ src/Footer.js
@@ -2,42 +2,42 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div style={{backgroundColor: "#003366", color: "white", border: "5px solid #003366"}}>
+    <div className="footer_container">
         <div>
-            <div className="row" style={{backgroundColor: "#003366", color: "white", padding: "3% 2% 0 2%"}}>
-                <div className="col-lg-6" style={{padding: "0 0 0 5%"}}>
+            <div className="row footer_row">
+                <div className="col-lg-6 footer_col">
                     <h3 className="text-uppercase mb-4 mt-3">Covid-19 India</h3>
-                    <p style={{fontSize: "16px"}}>
+                    <p className="footer_ul">
                         Data is collected from various sources & It changes rapidly.
-                        This data changes rapidly, so what’s shown may be out of date. Table values may not always be 100% accurate. Information about reported cases is also available on the <a href="https://www.who.int/" style={{textDecoration: "none", color: "white"}}>World Health Organization</a>  & <a href="https://www.worldometers.info/" style={{textDecoration: "none", color: "white"}}>worldoMeters.info</a>
+                        This data changes rapidly, so what’s shown may be out of date. Table values may not always be 100% accurate. Information about reported cases is also available on the <a href="https://www.who.int/">World Health Organization</a>  & <a href="https://www.worldometers.info/">worldoMeters.info</a>
                     </p>
                 </div>
-                <hr style={{color: "black", borderTop: "1px solid gray"}} />
-                <div className="col-lg-6" style={{padding: "0 0 0 5%"}}>
+                <hr className="hline" />
+                <div className="col-lg-6 footer_col">
                     <h3 className="text-uppercase mb-4 mt-3">
                         Important Links:
                     </h3>
-                    <ul style={{fontSize: "16px"}}>
+                    <ul className="footer_ul">
                         <li>
-                            Download the Aarogya Setu App  <img src={process.env.PUBLIC_URL + '/arogya.png'} alt="logo" style={{width: "25px"}} /> For 
-                            <a href="https://play.google.com/store/apps/details?id=nic.goi.aarogyasetu&hl=en_GB" style={{textDecoration: "none", color: "yellow"}}> Android <img src={process.env.PUBLIC_URL + '/android.png'} alt="logo" style={{width: "25px"}} /> </a>  For <a href="https://apps.apple.com/in/app/aarogyasetu/id1505825357" style={{textDecoration: "none", color: "yellow"}}>iPhone <img src={process.env.PUBLIC_URL + '/apple.png'} alt="logo"  style={{width: "25px", height: "25px"}}/></a>
+                            Download the Aarogya Setu App  <img className="footer_arogya_img" src={process.env.PUBLIC_URL + '/arogya.png'} alt="logo" /> For 
+                            <a className="footer_arogya_a" href="https://play.google.com/store/apps/details?id=nic.goi.aarogyasetu&hl=en_GB"> Android <img className="footer_arogya_img" src={process.env.PUBLIC_URL + '/android.png'} alt="logo" /> </a>  For <a className="footer_arogya_a" href="https://apps.apple.com/in/app/aarogyasetu/id1505825357">iPhone <img className="footer_arogya_img" src={process.env.PUBLIC_URL + '/apple.png'} alt="logo" /></a>
                         </li>
                         <li>
-                            <a href="https://web2.eu5.org/awarness_material/awarness_eng_hind.pdf" style={{textDecoration: "none", color: "white"}}>Guidelines for Reducing the Risk of Novel Coronavirus COVID-19 </a>
+                            <a className="footer_a" href="https://web2.eu5.org/awarness_material/awarness_eng_hind.pdf">Guidelines for Reducing the Risk of Novel Coronavirus COVID-19 </a>
                         </li>
                         <li>
-                            <a href="https://www.youtube.com/watch?v=ykZJ-jcE-Do&feature=youtu.be" style={{textDecoration: "none", color: "white"}}>Video on Covid-19 awareness for community, Hindi</a>
+                            <a className="footer_a" href="https://www.youtube.com/watch?v=ykZJ-jcE-Do&feature=youtu.be">Video on Covid-19 awareness for community, Hindi</a>
                         </li>
                         <li>
-                            <a href="https://web2.eu5.org/pass.php" style={{textDecoration: "none", color: "white"}}>Apply for Movement Pass During Covid-19 Lockdown</a>
+                            <a className="footer_a" href="https://web2.eu5.org/pass.php">Apply for Movement Pass During Covid-19 Lockdown</a>
                         </li>
                         <li>
-                            <a href="https://prixest.com/app/login.php" style={{textDecoration: "none", color: "white"}}>Covid_19 (Coronavirus) Awarness Quiz</a>
+                            <a className="footer_a" href="https://prixest.com/app/login.php">Covid_19 (Coronavirus) Awarness Quiz</a>
                         </li>
                     </ul>
                 </div>
             </div>
-            <hr style={{color: "black", borderTop: "1px solid gray"}} />
+            <hr className="hline"/>
             <div className="text-center">
                 <ul className="list-unstyled list-inline">
                     <li className="list-inline-item">
@@ -52,9 +52,11 @@ const Footer = () => {
                     </li>
                 </ul>
             </div>
-            <hr style={{color: "black", borderTop: "1px solid gray"}} />
+            <hr className="hline"/>
         </div>
-        <div style={{marginLeft: "47%", color: "white"}}>
+        <div className="copyright">
+            Made with &lt;3 by <a href="https://www.linkedin.com/in/nitin-porwal-9162a612a" >Nitin Porwal</a>
+            <br/>
             &copy; {new Date().getFullYear()} Copyright
         </div>
     </div>
