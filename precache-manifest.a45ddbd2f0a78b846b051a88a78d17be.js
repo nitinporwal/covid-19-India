4ee475fc95648dfc8682ae36a9afcfc64a47a972
nitin-codr <nitinporwal99@gmail.com>
2020-06-12 17:48:21 +0530

    Updates

---
+++ precache-manifest.a45ddbd2f0a78b846b051a88a78d17be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0dfbf0f7d3f81d5ef4f3dba5ed68816",
+    "url": "/covid19india/index.html"
+  },
+  {
+    "revision": "cafd0db7cf7c81976b25",
+    "url": "/covid19india/static/css/main.62bc4835.chunk.css"
+  },
+  {
+    "revision": "1b1925c8d3d796e83469",
+    "url": "/covid19india/static/js/2.ccfb4172.chunk.js"
+  },
+  {
+    "revision": "a5d8409f744083cea1f5d50d48943f15",
+    "url": "/covid19india/static/js/2.ccfb4172.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cafd0db7cf7c81976b25",
+    "url": "/covid19india/static/js/main.b3a94550.chunk.js"
+  },
+  {
+    "revision": "2eadd3fd7f41b1fe99fd",
+    "url": "/covid19india/static/js/runtime-main.16a2c883.js"
+  }
+]);
